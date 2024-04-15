127a94e0aedb71a11e5461953c8679774b7813b2
Sebastian Schirle <60340491+AboveYou@users.noreply.github.com>
2024-04-15 23:33:10 +0200

    automate VSC install

---
+++ code/init.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo -e "[!] installing code"
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+dnf check-update
+sudo dnf install code
+
+echo -e "\n[!] installing extensions"
+cat $(dirname $0)/extensions.txt | xargs -L 1 code --install-extension
